6f3d03912779a88306a12575705c1aecd40b6ef9
svetlilaloli <72747194+svetlilaloli@users.noreply.github.com>
2021-04-30 07:43:50 +0100

    Initial commit

---
+++ Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; private set; }
+        public int Year { get; private set; }
+        public IReadOnlyList<string> Authors { get; private set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            Title = title;
+            Year = year;
+            Authors = authors;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Year}";
+        }
+        public int CompareTo(Book other)
+        {
+            int result = Year.CompareTo(other.Year);
+            if (result == 0)
+            {
+                result = Title.CompareTo(other.Title);
+            }
+            return result;
+        }
+    }
+}
---
+++ Library.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                Reset();
+                this.books = new List<Book>(books);
+                this.books.Sort((x, y) => new BookComparator().Compare(x, y));
+            }
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext()
+            {
+                return ++currentIndex < books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+    }
+}
